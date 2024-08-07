291018d9016077f03df9cb24af105246027990d4
Vikash Kumar <vikasthakur9310@gmail.com>
2024-08-07 14:01:01 +0530

    add

--- src/App.js
+++ src/App.js
@@ -11,10 +11,11 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout />} />
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact/> } />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
