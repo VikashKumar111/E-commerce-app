e8033dc090e9457bfd14add3c24c7a4ca4a1b54d
Vikash Kumar <vikasthakur9310@gmail.com>
2024-08-08 15:52:29 +0530

    add

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import "./Header.css";
-import { NavLink,Link } from 'react-router-dom';
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
-      <header className='header-top-strip py-3'>
-        <div className='container-xxl'>
-          <div className='row'>
-            <div className='col-6'><p>Free Shipping over $100 $ Free Returns</p></div>
-            <div className='col-6'><p>Contact: <a href='tel: +91 9999999999'> +91 9999999999</a></p></div>
-         </div>
+      <header className="header-top-strip py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p className="text-white">Free Shipping over $100 $ Free Returns</p>
+            </div>
+            <div className="col-6">
+              <p className="text-end text-white">
+                Contact: <a href="tel: +91 9999999999"> +91 9999999999</a>
+              </p>
+            </div>
+          </div>
         </div>
       </header>
     </>
-  )
-}
+  );
+};
 
 export default Header;
