6cc090256d94a84d425946c41da586000fa3dcce
Vikash Kumar <vikasthakur9310@gmail.com>
2024-08-09 17:19:07 +0530

    add

--- src/components/Header.js
+++ src/components/Header.js
@@ -47,25 +47,25 @@ const Header = () => {
             <div className="col-5">
               <div className="header-upper-links d-flex align-items-center justify-content-between">
                 <div>
-                  <Link>
+                  <Link className="d-flex align-items-center gap-10 text-white">
                     <img src="images/compare.svg" alt="compare" />
-                    <p>
+                    <p className="mb-0">
                       Compare <br /> Products
                     </p>
                   </Link>
                 </div>
                 <div>
-                  <Link>
+                  <Link className="d-flex align-items-center gap-10 text-white">
                     <img src="images/wishlist.svg" alt="wishlist" />
-                    <p>
+                    <p className="mb-0">
                       Favourite <br /> Wishlist
                     </p>
                   </Link>
                 </div>
                 <div>
-                  <Link>
+                  <Link className="d-flex align-items-center gap-10 text-white">
                     <img src="images/user.svg" alt="user" />
-                    <p>
+                    <p className="mb-0">
                       Login <br /> My Account
                     </p>
                   </Link>
@@ -73,11 +73,30 @@ const Header = () => {
                 <div>
                   <Link className="d-flex align-items-center gap-10 text-white">
                     <img src="images/cart.svg" alt="cart" />
-                    <div className="d-flex flex-column">
+                    <div className="d-flex flex-column gap-10">
                       <span className="badge bg-white text-dark">0</span>
-                      <p>$ 500</p>
+                      <p className="mb-0">$ 500</p>
                     </div>
                   </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-bottom py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="menu-bottom d-flex align-items-center">
+                <div></div>
+                <div className="menu-links">
+                  <div className="d-flex align-items-center gap-15">
+                    <NavLink className="text-white" to="/">Home</NavLink>
+                    <NavLink to="/">Our Store</NavLink>
+                    <NavLink to="/">Blogs</NavLink>
+                    <NavLink to="/contact">Contact</NavLink>
+                  </div>
                 </div>
               </div>
             </div>
