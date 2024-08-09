1b6b7c6d110e10a26ab0d9c791c05e3620aaacbc
Vikash Kumar <vikasthakur9310@gmail.com>
2024-08-09 18:19:41 +0530

    add

--- src/components/Header.js
+++ src/components/Header.js
@@ -88,11 +88,44 @@ const Header = () => {
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
-              <div className="menu-bottom d-flex align-items-center">
-                <div></div>
+              <div className="menu-bottom d-flex align-items-center gap-30">
+                <div>
+                  <div  className="dropdown">
+                    <button
+                      className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex align-items-center"
+                      type="button"
+                      id="dropdownMenuButton1"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      <img src="images/menu.svg" alt="menu" />
+                      <span className="me-5 d-inline-block">Shop Categories</span>
+                    </button>
+                    <ul
+                       className="dropdown-menu"
+                      aria-labelledby="dropdownMenuButton1"
+                    >
+                      <li>
+                        <a  className="dropdown-item" href="#">
+                          Action
+                        </a>
+                      </li>
+                      <li>
+                        <a  className="dropdown-item" href="#">
+                          Another action
+                        </a>
+                      </li>
+                      <li>
+                        <a  className="dropdown-item" href="#">
+                          Something else here
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
                 <div className="menu-links">
                   <div className="d-flex align-items-center gap-15">
-                    <NavLink className="text-white" to="/">Home</NavLink>
+                    <NavLink to="/">Home</NavLink>
                     <NavLink to="/">Our Store</NavLink>
                     <NavLink to="/">Blogs</NavLink>
                     <NavLink to="/contact">Contact</NavLink>
