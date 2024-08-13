84b35512873bfaff4d89dfc439cfd61177a56629
Vikash Kumar <vikasthakur9310@gmail.com>
2024-08-13 11:36:38 +0530

    add

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,32 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <>
-      <footer className='py-3'></footer>
+      <footer className='py-3'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-5'>
+              <div className='footer-top-data d-flex gap-30 align-items-center'>
+                <img src='images/newsletter.png' alt='newsletter' />
+                <h2 className='mb-0 text-white'>Sign Up for Newsletter</h2>
+              </div>
+            </div>
+            <div className='col-7'>
+               <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Your Email Address"
+                  aria-label="Your Email Address"
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-3" id="basic-addon2">
+                  Subscribe
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
       <footer className='py-3'></footer>
       <footer className='py-4'>
         <div className='container-xxl'>
