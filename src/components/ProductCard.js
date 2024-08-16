685cb27a1f3039bfa4378fe3193fe10b2b6efb0c
Vikash Kumar <vikasthakur9310@gmail.com>
2024-08-16 16:36:23 +0530

    add

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,8 +4,14 @@ const ProductCard = () => {
     return (
         <div className="col-3">
             <div className="product-card">
-                <div className="product-image"></div>
-                <div className="product-details"></div>
+                <div className="product-image">
+                    <img  src="images/watch.jpg" alt="product image"/>
+                </div>
+                <div className="product-details">
+                    <h6 className="brand">Havels</h6>
+                    <h5 className="product-title">Kids headphones bulk 10 pack multi colored for students</h5>
+                    <p className="price">$100.00</p>
+                </div>
             </div>
         </div>
     )
