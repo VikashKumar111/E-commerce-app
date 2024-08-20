17d8f56f831b897d2e06a8711e9565ef4f11e50c
Vikash Kumar <vikasthakur9310@gmail.com>
2024-08-20 10:50:07 +0530

    add

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,9 +1,14 @@
 import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
+import {Helmet} from "react-helmet";
 
 const OurStore = () => {
     return (
-        <>
+        <>  
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>My Title</title>
+            </Helmet>
             <BreadCrumb title="our store" />
         </>
     )
