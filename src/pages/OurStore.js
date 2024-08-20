1a2e58d07794f62c65154e7b8a25b6f8c42ce4c3
Vikash Kumar <vikasthakur9310@gmail.com>
2024-08-20 11:44:37 +0530

    add

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,17 +1,36 @@
 import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
-import {Helmet} from "react-helmet";
+import Meta from "../components/Meta";
+
 
 const OurStore = () => {
-    return (
-        <>  
-            <Helmet>
-                <meta charSet="utf-8" />
-                <title>My Title</title>
-            </Helmet>
-            <BreadCrumb title="our store" />
-        </>
-    )
+  return (
+    <>
+      <Meta title="Our Store"/>
+          <BreadCrumb title="our store" />
+          <div className="store-wrapper home-wrapper-2 py-5">
+              <div className="container-xxl">
+                  <div className="row">
+                      <div className="col-3">
+                          <div className="filter-card mb-3">
+                              <h3 className="filter-title">Shop By Categories</h3>
+                          </div>
+                          <div className="filter-card mb-3">
+                                <h3 className="filter-title">Filter By</h3>
+                          </div>
+                          <div className="filter-card mb-3">
+                                <h3 className="filter-title">Product Tags</h3>
+                          </div>
+                          <div className="filter-card mb-3">
+                                <h3 className="filter-title">Random Products</h3>
+                          </div>
+                      </div>
+                      <div className="col-9"></div>
+                  </div>
+              </div>
+          </div>
+    </>
+  );
 };
 
 export default OurStore;
