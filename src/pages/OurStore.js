c34a8c61825672d36d44dfcbe4b0169618b5d7a2
Vikash Kumar <vikasthakur9310@gmail.com>
2024-08-20 13:46:50 +0530

    add

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,34 +1,104 @@
 import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
-
+import ReactStars from "react-rating-stars-component";
 
 const OurStore = () => {
   return (
     <>
-      <Meta title="Our Store"/>
-          <BreadCrumb title="our store" />
-          <div className="store-wrapper home-wrapper-2 py-5">
-              <div className="container-xxl">
-                  <div className="row">
-                      <div className="col-3">
-                          <div className="filter-card mb-3">
-                              <h3 className="filter-title">Shop By Categories</h3>
-                          </div>
-                          <div className="filter-card mb-3">
-                                <h3 className="filter-title">Filter By</h3>
-                          </div>
-                          <div className="filter-card mb-3">
-                                <h3 className="filter-title">Product Tags</h3>
-                          </div>
-                          <div className="filter-card mb-3">
-                                <h3 className="filter-title">Random Products</h3>
-                          </div>
-                      </div>
-                      <div className="col-9"></div>
-                  </div>
+      <Meta title="Our Store" />
+      <BreadCrumb title="our store" />
+      <div className="store-wrapper home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Shop By Categories</h3>
+                <div>
+                  <ul className="ps-0">
+                    <li>Watch</li>
+                    <li>Tv</li>
+                    <li>Camera</li>
+                    <li>Laptop</li>
+                  </ul>
+                </div>
               </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Filter By</h3>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Product Tags</h3>
+                <div>
+                  <div className="product-tags d-flex flex-wrap align-items-center gap-10">
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Headphone
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Mobile
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Laptop
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      wire
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Random Products</h3>
+                <div>
+                  <div className="random-products mb-3 d-flex">
+                    <div className="w-50">
+                      <img
+                        src="images/watch.jpg"
+                        className="img-fluid"
+                        alt="watch"
+                      />
+                    </div>
+                    <div className="w-50">
+                      <h5>
+                        Kids headphones bulk 10 pack multi colored for students
+                      </h5>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value="3"
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <b>$ 300</b>
+                    </div>
+                  </div>
+                  <div className="random-products d-flex">
+                    <div className="w-50">
+                      <img
+                        src="images/watch.jpg"
+                        className="img-fluid"
+                        alt="watch"
+                      />
+                    </div>
+                    <div className="w-50">
+                      <h5>
+                        Kids headphones bulk 10 pack multi colored for students
+                      </h5>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value="3"
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <b>$ 300</b>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-9"></div>
           </div>
+        </div>
+      </div>
     </>
   );
 };
