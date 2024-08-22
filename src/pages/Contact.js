4f18bb40fbefae722f0a2c4c7debb92422196df7
Vikash Kumar <vikasthakur9310@gmail.com>
2024-08-22 16:19:40 +0530

    add

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Meta from "../components/Meta";
+import BreadCrumb from "../components/BreadCrumb";
 
 const Contact = () => {
-  return <h1>Contact</h1>;
+  return <>
+      <Meta title="Contact Us" />
+    <BreadCrumb title="Contact Us" />
+    <div className="contact-wrapper py-5 home-wrapper-2">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-12"></div>
+        </div>
+      </div>
+    </div>
+  </>;
 };
 
 export default Contact;
