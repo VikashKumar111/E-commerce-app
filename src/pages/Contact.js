88098787a870e7fe30f254069eecdb6ae1217dad
Vikash Kumar <vikasthakur9310@gmail.com>
2024-08-22 17:36:06 +0530

    add

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,17 +3,39 @@ import Meta from "../components/Meta";
 import BreadCrumb from "../components/BreadCrumb";
 
 const Contact = () => {
-  return <>
+  return (
+    <>
       <Meta title="Contact Us" />
-    <BreadCrumb title="Contact Us" />
-    <div className="contact-wrapper py-5 home-wrapper-2">
-      <div className="container-xxl">
-        <div className="row">
-          <div className="col-12"></div>
+      <BreadCrumb title="Contact Us" />
+      <div className="contact-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d55289.14317113351!2d77.53488298554187!3d29.991743799999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1724324570217!5m2!1sen!2sin"
+                width="600"
+                height="450"
+                className="border-0 w-100"
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
+            <div className="col-12 mt-5">
+              <div className="contact-inner-wrapper d-flex justify-content-between">
+                <div>
+                  <h3 className="contact-title mb-4">Contact</h3>
+                </div>
+                <div>
+                  <h3 className="contact-title mb-4">Get In Touch With Us</h3>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </>;
+    </>
+  );
 };
 
 export default Contact;
