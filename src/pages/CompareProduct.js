9a64ba3d9f1d59140c0a46e46b8808bbd2dce04f
Vikash Kumar <vikasthakur9310@gmail.com>
2024-08-23 15:26:12 +0530

    add

--- src/pages/CompareProduct.js
+++ src/pages/CompareProduct.js
@@ -1,10 +1,32 @@
 import React from "react";
-
+import Meta from "../components/Meta";
+import BreadCrumb from "../components/BreadCrumb";
 
 const CompareProduct = () => {
-    return (
-        <div>Hello</div>
-    )
+  return (
+    <>
+      <Meta title="Our Store" />
+      <BreadCrumb title="our store" />
+      <div className="compare-product-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+              <div className="compare-product-card position-relative">
+                <img
+                  src="images/cross.svg"
+                  alt="cross"
+                  className="position-absolute cross"
+                />
+                <div className="product-card-image">
+                  <img src="images/watch.jpg" alt="watch" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CompareProduct;
