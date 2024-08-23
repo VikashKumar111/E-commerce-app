7b1664ef89131f23d0a902f3629a1428d480af2c
Vikash Kumar <vikasthakur9310@gmail.com>
2024-08-23 12:11:00 +0530

    add

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Meta from "../components/Meta";
 import BreadCrumb from "../components/BreadCrumb";
+import { AiOutlineHome, AiOutlineMail } from "react-icons/ai";
+import { BiInfoCircle, BiPhoneCall } from "react-icons/bi";
 
 const Contact = () => {
   return (
@@ -27,16 +29,35 @@ const Contact = () => {
                   <h3 className="contact-title mb-4">Contact</h3>
                   <form action="" className="d-flex flex-column gap-15">
                     <div>
-                      <input type="text" className="form-control" placeholder="Name"/>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Name"
+                      />
                     </div>
-                     <div>
-                      <input type="email" className="form-control" placeholder="Email"/>
+                    <div>
+                      <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Email"
+                      />
                     </div>
-                     <div>
-                      <input type="tel" className="form-control" placeholder="Mobile Number"/>
+                    <div>
+                      <input
+                        type="tel"
+                        className="form-control"
+                        placeholder="Mobile Number"
+                      />
                     </div>
-                     <div>
-                      <textarea name="" id="" className="w-100 form-control" placeholder="Comment" cols="30" rows="4"></textarea>
+                    <div>
+                      <textarea
+                        name=""
+                        id=""
+                        className="w-100 form-control"
+                        placeholder="Comment"
+                        cols="30"
+                        rows="4"
+                      ></textarea>
                     </div>
                     <div>
                       <button className="button border-0">Submit</button>
@@ -45,6 +66,28 @@ const Contact = () => {
                 </div>
                 <div>
                   <h3 className="contact-title mb-4">Get In Touch With Us</h3>
+                  <div>
+                    <ul className="ps-0">
+                      <li className="mb-3 d-flex gap-15 align-items-center justify-content-center">
+                        <AiOutlineHome className="fs-5 d-flex justify-content-center" />
+                        <address className="mb-0">
+                          456 Railway Road, Near Saharanpur Junction, Saharanpur, Uttar Pradesh, 247001
+                        </address>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <BiPhoneCall className="fs-5" />
+                        <a href="tel:+91 9999999999" className="text-dark">8888888888</a>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <AiOutlineMail className="fs-5" />
+                        <a href="mailto:bme@gmail.com" className="text-dark">bme@gmail.com</a>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <BiInfoCircle className="fs-5" />
+                        <p className="mb-0">Monday - Friday 10 AM to 8 PM</p>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
