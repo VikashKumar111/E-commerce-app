8aef4da45ebda4ae2e930b89a25444e2ff5b3061
Vikash Kumar <vikasthakur9310@gmail.com>
2024-09-12 12:05:19 +0530

    add

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,23 +1,34 @@
-import React from 'react'
-import Meta from '../components/Meta'
-import BreadCrumb from '../components/BreadCrumb'
+import React from "react";
+import Meta from "../components/Meta";
+import BreadCrumb from "../components/BreadCrumb";
 
 const Cart = () => {
   return (
-      <>
-          <Meta title="Cart" />                                               
-          <BreadCrumb title="Cart" />
-          <div className='cart-wrapper home-wrapper-2 py-5'>
-              <div className='container-xxl'>
-                  <div className='row'>
-                      <div className='col-12'>
-                          <div className='cart-header d-flex justify-content-between align-content-center'></div>
-                      </div>
-                  </div>
+    <>
+      <Meta title="Cart" />
+      <BreadCrumb title="Cart" />
+      <div className="cart-wrapper home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="cart-header py-3 d-flex justify-content-between align-content-center">
+                <h4 className="cart-col-1">Product</h4>
+                <h4 className="cart-col-2">Price</h4>
+                <h4 className="cart-col-3">Quantity</h4>
+                <h4 className="cart-col-4">Total</h4>
               </div>
+              <div className="cart-data py-3 d-flex justify-content-between align-content-center">
+                <div className="cart-col-1"></div>
+                <div className="cart-col-2"></div>
+                <div className="cart-col-3"></div>
+                <div className="cart-col-4"></div>
+              </div>
+            </div>
           </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
