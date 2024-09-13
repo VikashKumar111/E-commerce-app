f316a996755c72fbf464312bea0f1ceee1a1a27b
Vikash Kumar <vikasthakur9310@gmail.com>
2024-09-13 13:28:13 +0530

    checkout

---
+++ src/pages/Checkout.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Checkout = () => {
+  return (
+    <>
+      <div className="checkout-wrapper">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-7">
+              <div className="checkout-left-data">
+                <h3 className="website-name">BME</h3>
+                <nav
+                  style={{"--bs-breadcrumb-divider" : ">"}}
+                  aria-label="breadcrumb"
+                >
+                  <ol className="breadcrumb">
+                    <li className="breadcrumb-item">
+                      <a href="#">Home</a>
+                    </li>
+                    <li className="breadcrumb-item active" aria-current="page">
+                      Library
+                    </li>
+                  </ol>
+                </nav>
+              </div>
+            </div>
+            <div className="col-5"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Checkout;
