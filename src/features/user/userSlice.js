bfe097e48cba7693598b53a7b7cfd2c3af0474bf
Vikash Kumar <vikasthakur9310@gmail.com>
2024-09-18 18:08:42 +0530

    add

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,10 +1,25 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 
+export const registerUser = createAsyncThunk("auth/register", async (thunkAPI) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+const initialState = {
+    user: "",
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message:""
+}
 
 export const authSlice = createSlice({
     name: "auth",
-    initialState: "",
+    initialState: initialState,
     reducers: {},
     extraReducers: (builder) => {
         builder.addCase()
