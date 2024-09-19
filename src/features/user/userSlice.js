a2398197481b28bf71635e65469c231a0896269f
Vikash Kumar <vikasthakur9310@gmail.com>
2024-09-19 17:05:55 +0530

    updates

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,11 +1,12 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { authService } from "./userService";
 
 
-export const registerUser = createAsyncThunk("auth/register", async (thunkAPI) => {
+export const registerUser = createAsyncThunk("auth/register", async (userData,thunkAPI) => {
     try {
-        
+        return await authService.register(userData)
     } catch (error) {
-        
+        return thunkAPI.rejectWithValue(error)
     }
 })
 
@@ -22,6 +23,19 @@ export const authSlice = createSlice({
     initialState: initialState,
     reducers: {},
     extraReducers: (builder) => {
-        builder.addCase()
+        builder.addCase(registerUser.pending, (state) => {
+            state.isLoading = true;
+        }).addCase(registerUser.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.isError = false;
+            state.isSuccess = true;
+        }).addCase(registerUser.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.isSuccess = false;
+        })
     }
 })
+
+
+export default authSlice.reducer;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -3,8 +3,31 @@ import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import Container from "../components/Container";
 import CustomInput from "../components/CustomInput";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+const signUpSchema = yup.object({
+  firstName: yup.string().required("First Name is Required"),
+  lastName: yup.string().required("Last Name is Required"),
+  email: yup.string().nullable().required("Email is Required"),
+  mobile: yup.string().required("Mobile Number is Required"),
+  password: yup.string().required("Password is Required"),
+});
 
 const Signup = () => {
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      mobile: "",
+      password: "",
+    },
+    validationSchema: signUpSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
     <>
       <Meta title="Sign Up" />
@@ -14,19 +37,66 @@ const Signup = () => {
           <div className="col-12">
             <div className="auth-card">
               <h3 className="text-center mb-3">Sign Up</h3>
-              <form action="" className="d-flex flex-column gap-15">
-                <CustomInput type="text" name="name" placeholder="Name" />
-                <CustomInput type="email" name="email" placeholder="Email" />
+              <form
+                action=""
+                onSubmit={formik.handleSubmit}
+                className="d-flex flex-column gap-15"
+              >
+                <CustomInput
+                  type="text"
+                  name="firstname"
+                  placeholder="First Name"
+                  value={formik.values.firstName}
+                  onChange={formik.handleChange("firstName")}
+                  onBlur={formik.handleBlur("firstName")}
+                />
+                <div className="error">
+                  {formik.touched.firstName && formik.errors.firstName}
+                </div>
+                <CustomInput
+                  type="text"
+                  name="lastname"
+                  placeholder="Last Name"
+                  value={formik.values.lastName}
+                  onChange={formik.handleChange("lastName")}
+                  onBlur={formik.handleBlur("lastName")}
+                />
+                <div className="error">
+                  {formik.touched.lastName && formik.errors.lastName}
+                </div>
+                <CustomInput
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={formik.values.email}
+                  onChange={formik.handleChange("email")}
+                  onBlur={formik.handleBlur("email")}
+                />
+                <div className="error">
+                  {formik.touched.email && formik.errors.email}
+                </div>
                 <CustomInput
                   type="tel"
                   name="mobile"
                   placeholder="Mobile Number"
+                  value={formik.values.mobile}
+                  onChange={formik.handleChange("mobile")}
+                  onBlur={formik.handleBlur("mobile")}
                 />
+                <div className="error">
+                  {formik.touched.mobile && formik.errors.mobile}
+                </div>
                 <CustomInput
                   type="password"
                   name="password"
                   placeholder="Password"
+                 value={formik.values.password}
+                  onChange={formik.handleChange("password")}
+                  onBlur={formik.handleBlur("password")}
                 />
+                <div className="error">
+                  {formik.touched.password && formik.errors.password}
+                </div>
                 <div>
                   <div className="mt-3 d-flex justify-content-center align-items-center gap-15">
                     <button type="submit" className="button border-0">
