3e2a3597432cede21de6bfb8014d56cc87540b1f
Vikash Kumar <vikasthakur9310@gmail.com>
2024-09-24 18:07:38 +0530

    add

--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -17,31 +17,38 @@ const Wishlist = () => {
     dispatch(getUserProductWishlist());
   };
 
-  const wishlistState = useSelector((state) => state.auth.wishlist.wishlist);
+  const wishlistState = useSelector((state) => state.auth.wishlist?.wishlist);
+  console.log(wishlistState);
+
   return (
     <>
       <Meta title="Wishlist" />
       <BreadCrumb title="wishlist" />
       <Container class1="wishlist-wrapper home-wrapper-2 py-5">
         <div className="row">
+          {/* {!wishlistState && <div>No Data</div>} */}
           {wishlistState?.map((item, index) => {
+            console.log(item?.images); // Check if images array exists
+            console.log(item?.images[0]?.url); // Check if the URL is valid
             return (
-              <div className="col-3">
+              <div className="col-3" key={index}>
                 <div className="wishlist-card position-relative">
                   <img
                     src={cross}
                     alt="cross"
                     className="position-absolute cross img-fluid"
                   />
-                  <div className="wishlist-card-image">
-                    <img src={watch} className="img-fluid w-100" alt="watch" />
+                  <div className="wishlist-card-image bg-white">
+                    <img
+                      src={item?.images[0]?.url ? item.images[0].url : watch}
+                      className="img-fluid w-100 d-block mx-auto"
+                      alt="watch"
+                      // width={160}
+                    />
                   </div>
                   <div className="py-3 px-3">
-                    <h5 className="title">
-                      Galaxy Infinity Pro - 6.5" Super AMOLED, 6GB RAM, 48MP
-                      Triple Camera
-                    </h5>
-                    <h6 className="price mb-3 mt-3">$ 100</h6>
+                    <h5 className="title">{item?.title}</h5>
+                    <h6 className="price mb-3 mt-3">$ {item?.price}</h6>
                   </div>
                 </div>
               </div>
