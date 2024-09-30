bd1fc2d774a6767443dec843315838f1a681985c
Vikash Kumar <vikasthakur9310@gmail.com>
2024-09-30 17:00:33 +0530

    updates

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,94 +4,122 @@ import BreadCrumb from "../components/BreadCrumb";
 import { AiOutlineHome, AiOutlineMail } from "react-icons/ai";
 import { BiInfoCircle, BiPhoneCall } from "react-icons/bi";
 import Container from "../components/Container";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { useDispatch } from "react-redux";
+
+
+const contactSchema = yup.object({
+  name: yup.string().required("Name is Required"),
+  email: yup.string().nullable().email("Email should be Valid").required("Email Address is Required"),
+  mobile: yup.string().default("").nullable().required("Mobile Number is Required"),
+  comment: yup.string().default("").nullable().required("Comment is Required"),
+});
+
+
+
 
 const Contact = () => {
+    const dispatch = useDispatch();
+    const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      mobile: "",
+      comment: "",
+    },
+    validationSchema: contactSchema,
+    onSubmit: (values) => {
+      // dispatch(registerUser(values));
+    },
+  });
   return (
     <>
       <Meta title="Contact Us" />
       <BreadCrumb title="Contact Us" />
       <Container class1="contact-wrapper py-5 home-wrapper-2">
-          <div className="row">
-            <div className="col-12">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d55289.14317113351!2d77.53488298554187!3d29.991743799999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1724324570217!5m2!1sen!2sin"
-                width="600"
-                height="450"
-                className="border-0 w-100"
-                allowFullScreen=""
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-              ></iframe>
-            </div>
-            <div className="col-12 mt-5">
-              <div className="contact-inner-wrapper d-flex justify-content-between">
-                <div>
-                  <h3 className="contact-title mb-4">Contact</h3>
-                  <form action="" className="d-flex flex-column gap-15">
-                    <div>
-                      <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Name"
-                      />
-                    </div>
-                    <div>
-                      <input
-                        type="email"
-                        className="form-control"
-                        placeholder="Email"
-                      />
-                    </div>
-                    <div>
-                      <input
-                        type="tel"
-                        className="form-control"
-                        placeholder="Mobile Number"
-                      />
-                    </div>
-                    <div>
-                      <textarea
-                        name=""
-                        id=""
-                        className="w-100 form-control"
-                        placeholder="Comment"
-                        cols="30"
-                        rows="4"
-                      ></textarea>
-                    </div>
-                    <div>
-                      <button className="button border-0">Submit</button>
-                    </div>
-                  </form>
-                </div>
-                <div>
-                  <h3 className="contact-title mb-4">Get In Touch With Us</h3>
+        <div className="row">
+          <div className="col-12">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d55289.14317113351!2d77.53488298554187!3d29.991743799999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1724324570217!5m2!1sen!2sin"
+              width="600"
+              height="450"
+              className="border-0 w-100"
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+          <div className="col-12 mt-5">
+            <div className="contact-inner-wrapper d-flex justify-content-between">
+              <div>
+                <h3 className="contact-title mb-4">Contact</h3>
+                <form action="" onSubmit={formik.handleSubmit} className="d-flex flex-column gap-15">
                   <div>
-                    <ul className="ps-0">
-                      <li className="mb-3 d-flex gap-15 align-items-center justify-content-center">
-                        <AiOutlineHome className="fs-5 d-flex justify-content-center" />
-                        <address className="mb-0">
-                          456 Railway Road, Near Saharanpur Junction, Saharanpur, Uttar Pradesh, 247001
-                        </address>
-                      </li>
-                      <li className="mb-3 d-flex gap-15 align-items-center">
-                        <BiPhoneCall className="fs-5" />
-                        <a href="tel:+91 9999999999">+91 8888888888</a>
-                      </li>
-                      <li className="mb-3 d-flex gap-15 align-items-center">
-                        <AiOutlineMail className="fs-5" />
-                        <a href="mailto:bme@gmail.com">bme@gmail.com</a>
-                      </li>
-                      <li className="mb-3 d-flex gap-15 align-items-center">
-                        <BiInfoCircle className="fs-5" />
-                        <p className="mb-0">Monday - Friday 10 AM to 8 PM</p>
-                      </li>
-                    </ul>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Name"
+                    />
                   </div>
+                  <div>
+                    <input
+                      type="email"
+                      className="form-control"
+                      placeholder="Email"
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="tel"
+                      className="form-control"
+                      placeholder="Mobile Number"
+                    />
+                  </div>
+                  <div>
+                    <textarea
+                      name=""
+                      id=""
+                      className="w-100 form-control"
+                      placeholder="Comment"
+                      cols="30"
+                      rows="4"
+                    ></textarea>
+                  </div>
+                  <div>
+                    <button className="button border-0">Submit</button>
+                  </div>
+                </form>
+              </div>
+              <div>
+                <h3 className="contact-title mb-4">Get In Touch With Us</h3>
+                <div>
+                  <ul className="ps-0">
+                    <li className="mb-3 d-flex gap-15 align-items-center justify-content-center">
+                      <AiOutlineHome className="fs-5 d-flex justify-content-center" />
+                      <address className="mb-0">
+                        456 Railway Road, Near Saharanpur Junction, Saharanpur,
+                        Uttar Pradesh, 247001
+                      </address>
+                    </li>
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <BiPhoneCall className="fs-5" />
+                      <a href="tel:+91 9999999999">+91 8888888888</a>
+                    </li>
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <AiOutlineMail className="fs-5" />
+                      <a href="mailto:bme@gmail.com">bme@gmail.com</a>
+                    </li>
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <BiInfoCircle className="fs-5" />
+                      <p className="mb-0">Monday - Friday 10 AM to 8 PM</p>
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
           </div>
+        </div>
       </Container>
     </>
   );
