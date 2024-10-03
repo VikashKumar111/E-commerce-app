93b18431a23c3f4075c682a1e78e4ae562ad4c27
Vikash Kumar <vikasthakur9310@gmail.com>
2024-10-03 12:03:48 +0530

    add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -243,10 +243,82 @@ const Home = () => {
           <div className="col-12">
             <h3 className="section-heading">Featured Collection</h3>
           </div>
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          <div className="row">
+            {productState &&
+              productState.map((item, index) => {
+                if (item.tags === "featured") {
+                  return (
+                    <div
+                      key={index}
+                      className={"col-3 mb-4"}
+                    >
+                      <Link
+                        // to={`${
+                        //   location.pathname === "/"
+                        //     ? "/product/:id"
+                        //     : location.pathname === "/product/:id"
+                        //     ? "/product/1"
+                        //     : ":id"
+                        // }`}
+                        className="product-card position-relative"
+                      >
+                        <div className="wishlist-icon position-absolute">
+                          <button
+                            className="border-0 bg-transparent"
+                            onClick={(e) => addToWish(item._id)}
+                          >
+                            <img src={wish} alt="wishlist" />
+                          </button>
+                        </div>
+                        <div className="product-image">
+                          <img
+                            src={item?.images[0].url}
+                            className="img-fluid"
+                            alt="product"
+                          />
+                          <img
+                            src={watch2}
+                            className="img-fluid"
+                            alt="product "
+                          />
+                        </div>
+                        <div className="product-details">
+                          <h6 className="brand">{item?.brand}</h6>
+                          <h5 className="product-title">{item?.title}</h5>
+                          <ReactStars
+                            count={5}
+                            size={24}
+                            value={item?.totalrating}
+                            edit={false}
+                            activeColor="#ffd700"
+                          />
+                          {/* <p
+                            className={`description ${
+                              grid === 12 ? "d-block " : "d-none"
+                            }`}
+                            dangerouslySetInnerHTML={{
+                              __html: item?.description,
+                            }}
+                          ></p> */}
+                          <p className="price">$ {item?.price}</p>
+                        </div>
+                        <div className="action-bar position-absolute d-flex flex-column gap-15">
+                          <button className="border-0 bg-transparent">
+                            <img src={prodcompare} alt="compare" />
+                          </button>
+                          <button className="border-0 bg-transparent">
+                            <img src={view} alt="view" />
+                          </button>
+                          <button className="border-0 bg-transparent">
+                            <img src={addcart} alt="addcart" />
+                          </button>
+                        </div>
+                      </Link>
+                    </div>
+                  );
+                }
+              })}
+          </div>
         </div>
       </Container>
       <Container class1="home-wrapper-2 py-5">
@@ -315,9 +387,6 @@ const Home = () => {
                   );
                 }
               })}
-            {/* <SpecialProduct />
-              <SpecialProduct />
-              <SpecialProduct /> */}
           </div>
         </div>
       </Container>
