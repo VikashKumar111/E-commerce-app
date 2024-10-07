fe095cf6eb0dcf5f50942db7b8e4de6028de1831
Vikash Kumar <vikasthakur9310@gmail.com>
2024-10-07 12:59:27 +0530

    add

--- src/features/user/userService.js
+++ src/features/user/userService.js
@@ -52,8 +52,32 @@ const getUserWishlist = async () => {
   }
 }
 
+
+const addToCart = async (cartData) => {
+   // Retrieve the token from localStorage
+    const token = localStorage.getItem("Token");
+
+    console.log("Token retrieved from localStorage:", token);
+
+    // If the token exists, include it in the headers
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    };
+    
+    // Log the config object to see what it's passing in the request
+    console.log("Config object:", config);
+  const response = await axios.post(`${base_url}user/cart`, cartData, config);
+  if (response.data) {
+    return response.data;
+  }
+}
+
 export const authService = {
   register,
   login,
   getUserWishlist,
+  addToCart,
 };
