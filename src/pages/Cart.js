86a3fb9d6851e60268399de581cf45798d19b307
Vikash Kumar <vikasthakur9310@gmail.com>
2024-10-08 15:42:00 +0530

    updates

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Meta from "../components/Meta";
 import BreadCrumb from "../components/BreadCrumb";
 import watch from "../images/watch.jpg";
 import { AiFillDelete } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import Container from "../components/Container";
+import { useDispatch } from "react-redux";
+import { getUserCart } from "../features/user/userSlice";
 
 const Cart = () => {
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUserCart());
+  }, []);
+  
   return (
     <>
       <Meta title="Cart" />
